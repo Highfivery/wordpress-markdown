4b421efbffb18dd0322e3ff90ea2bb16938addf0
Ben Marshall <bmarshall511@gmail.com>
2020-08-18 09:26:12 -0500

    Updates

--- parse-markdown.php
+++ parse-markdown.php
@@ -13,25 +13,25 @@
  * Text Domain: parse-markdown
  */
 
-define( 'MARKDOWN', __FILE__ );
+define( 'PARSE_MARKDOWN', __FILE__ );
 
 /**
  * Include required dependencies.
  */
-require plugin_dir_path( MARKDOWN ) . 'vendor/autoload.php';
+require plugin_dir_path( PARSE_MARKDOWN ) . 'vendor/autoload.php';
 
 /**
  * Parse comment text.
  *
  * @since 1.0.0
  *
- * @param string $comment_text Text of the current comment.
+* @param string $comment_text Text of the current comment.
  * @param WP_Comment|null $comment The comment object. Null if not found.
  * @return string The parsed comment.
  */
-function markdown_markup_comment_text( $comment_text, $comment = null ) {
+function parse_markdown_comment_text( $comment_text, $comment = null ) {
   $Parsedown = new Parsedown();
 
   return $Parsedown->line( $comment_text );
 }
-add_filter( 'comment_text', 'markdown_markup_comment_text', 10, 2 );
+add_filter( 'comment_text', 'parse_markdown_comment_text', 10, 2 );
